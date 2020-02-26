fde9ccee6bda9fe564b28c984f260360489e7661
Atekawere <jacintawere2@gmail.com>
2020-02-26 11:53:38 +0300

    used jqurry in making hovers

--- script.js
+++ script.js
@@ -26,7 +26,31 @@ $(document).ready(function(){
         var email = $("input#email").val();
         var message = $("input#message").val();
         alert (" Hi " + name + ",we have received your message,thank you for communicating to us.");
-    })
+    });
 
-})
+    $("#image").hover(function(){
+        $("#move").toggle();
+    });
+    $("#kit").hover(function(){
+        $("#fop").toggle();
+    });
+    $("#ten").hover(function(){
+        $("#toy").toggle();
+    });
+    $("#jin").hover(function(){
+        $("#gut").toggle();
+    });
+    $("#hit").hover(function(){
+        $("#lul").toggle();
+    });
+    $("#moji").hover(function(){
+        $("#pol").toggle();
+    });
+    $("#jer").hover(function(){
+        $("#rej").toggle();
+    });
+    $("#yet").hover(function(){
+        $("#tye").toggle();
+    });
+});
 
